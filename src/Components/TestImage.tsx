8742215fb102ed33083f4aff2203079616f4ad25
Remzymiles <remzymiles@gmail.com>
2024-05-03 19:45:11 +0100

    fixed the filters bug

--- src/Components/TestImage.tsx
+++
@@ -1,7 +0,0 @@
-import Picture from '../../public/images/12-Antonellis-Cir-1.webp'
-
-export const TestPicture = () => {
-  return (
-    <img src={Picture} alt="library picture" className='h-[100%] w-[100%]' />
-  )
-}
