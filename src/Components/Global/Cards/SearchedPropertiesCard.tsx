8eda2c1524db92a21c3e34954b702070b4f4ac2c
Remzymiles <remzymiles@gmail.com>
2024-05-19 15:39:57 +0100

    implemented dark mode for the search page

--- src/Components/Global/Cards/SearchedPropertiesCard.tsx
+++ src/Components/Global/Cards/SearchedPropertiesCard.tsx
@@ -82,26 +82,26 @@ export const SearchedPropertiesCard = () => {
                   <h1 className="font-bold tracking-wider">
                     ${property.price.toLocaleString()}
                   </h1>
-                  <p className="text-[11px] bg-primaryColor-cream m-1 py-[2px] px-1 rounded-md uppercase">
+                  <p className="text-[11px] bg-primaryColor-cream text-black m-1 py-[2px] px-1 rounded-md uppercase">
                     for sale
                   </p>
                 </div>
                 <div className="mt-1 flex gap-3">
-                  <span className="flex gap-1 text-sm text-secondaryColor-dark">
+                  <span className="flex gap-1 text-sm text-secondaryColor-dark dark:text-gray-400">
                     <BedIcon extraStyle="text-gray-500 text-[15px]" />{" "}
                     <span className="font-extrabold">
                       {property.details.beds}
                     </span>
                     bd
                   </span>
-                  <span className="flex gap-1 text-sm text-secondaryColor-dark">
+                  <span className="flex gap-1 text-sm text-secondaryColor-dark dark:text-gray-400">
                     <BathIcon extraStyle="text-gray-500 text-[15px]" />{" "}
                     <span className="font-extrabold">
                       {property.details.baths}
                     </span>
                     ba
                   </span>
-                  <span className="flex gap-1 text-sm text-secondaryColor-dark">
+                  <span className="flex gap-1 text-sm text-secondaryColor-dark dark:text-gray-400">
                     <SquareFootIcon extraStyle="fill-gray-500 w-[20px] h-[20px] mt-[2px]" />
                     <span className="font-extrabold">
                       {property.details.sqft.toLocaleString()}
@@ -128,7 +128,7 @@ export const SearchedPropertiesCard = () => {
               <HeartIcon
                 color={`${
                   wishlistPropertyIds.includes(property.property_id)
-                    ? "text-primaryColor-light"
+                    ? "text-primaryColor-light dark:text-primaryColorDarkMode"
                     : "text-white"
                 }`}
               />
